8f2291afb3ac8ebf88e417287bb55c293a812213
Dickson Prince <dicksoniprince@gmail.com>
2019-09-19 07:34:04 +0100

    Added new page

--- public/js/source.js
+++ public/js/source.js
@@ -1,5 +1,8 @@
 $( document ).ready(function() {
   $("#update").hide()
+  $(".hide-form").hide()
+  $.fn.myFunction = function(){  
+}
 $.ajax({
   url: 'http://localhost:3004/products',
   method: 'get',
@@ -70,6 +73,11 @@ $.ajax({
                   })
     })
 
+    $('#create').on('click',()=>{
+      $(".hide-form").show()
+      $('#create').hide()
+    })
+
 
 
     $('#update').on('click',(event)=>{
@@ -100,10 +108,6 @@ location.reload(true);
         })
 })
 
-
-
-
-
   $('.delete-btn').on('click',(event)=>{
     if (!confirm("Sure you want to delete this record?")){
       return false;
@@ -123,8 +127,11 @@ location.reload(true);
 
 
     $('.edit-btn').on('click',(event)=>{
+      //alert("CALLING")
       let p = event.target.id.split('edt-').join('')
       newid = event.target.id.split('edt-').join('')
+      $(".hide-form").show()
+      $(window).scrollTop(0);
       $("#save").hide()
       $("#update").show()
          
@@ -149,7 +156,7 @@ location.reload(true);
 
     $('#search').on('click',(event)=>{
       //let p = event.target.id.split('edt-').join('')
-      newid = event.target.id.split('edt-').join('')
+      //newid = event.target.id.split('edt-').join('')
          
         $.ajax({
                 url:`http://localhost:3004/products`,
@@ -157,7 +164,7 @@ location.reload(true);
               })
               .done((event)=>{
                 
-                $("#table_items tbody tr").remove();
+                $("#tbody tr").remove();
 
                 for(let i = 0; i < event.length; i++){
                     if(event[i].name == $('#search-text').val() || event[i].brand == $('#search-text').val()){
@@ -173,11 +180,11 @@ location.reload(true);
                             <td>${event[i].colour}</td>
                             <td>${event[i].screensize}</td>
                             <td>
-                                <button id="edt-${event[i].id}" class="edit-btn btn btn-primary btn-sm">Edit</button>
-                                <button id="del-${event[i].id}" class="delete-btn btn btn-danger btn-sm">Delete</button>
+                                <button id="edt-${event[i].id}" class="tryedit btn btn-primary btn-sm">Edit</button>
+                                <button id="del-${event[i].id}" class=" btn btn-danger btn-sm">Delete</button>
                             </td>
                         </tr>`
-                                )
+                      )
                       
 
                       // $('#brand').val()
@@ -187,6 +194,32 @@ location.reload(true);
                     }
                 }
               })
+    })
+
+    $('.tryedit').on('click',(event)=>{
+      alert("CALL ME")
+      // let p = event.target.id.split('edt-').join('')
+      // newid = event.target.id.split('edt-').join('')
+      // $(".hide-form").show()
+      // $(window).scrollTop(0);
+      // $("#save").hide()
+      // $("#update").show()
+         
+      //   $.ajax({
+      //           url:`http://localhost:3004/products`,
+      //           method: 'get'
+      //         })
+      //         .done((event)=>{
+      //           for(let i = 0; i < event.length; i++){
+      //               if(event[i].id == p){
+      //                 $('#name').val(`${event[i].name}`)
+      //                 $('#brand').val(`${event[i].brand}`)
+      //                 $('#price').val(`${event[i].price}`)
+      //                 $('#colour').val(`${event[i].colour}`)
+      //                 $('#screensize').val(`${event[i].screensize}`)
+      //               }
+      //           }
+      //         })
     })
 
     
